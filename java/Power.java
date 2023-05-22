70ddf2046978291c017473d3125a3beec5665995
Murugadass-b <murugadassbala@gmail.com>
2023-05-22 20:31:42 +0530

    30 th problem added

---
+++ java/Power.java
@@ -0,0 +1,16 @@
+//30
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String args){
+        Scanner sc = new Scanner(System.in);
+        double base;
+        System.out.print("Enter the value of base:");
+        base = sc.nextDouble();
+        System.out.print("Enter the value of exponent: ");
+        double expo = sc.nextDouble();
+        double res = Math.pow(base, expo);
+        System.out.println(base+" ^ "+expo+" : "+res);
+        sc.close();
+    }
+}

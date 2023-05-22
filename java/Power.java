2af5a2c718754d1f0c5f2914ba0f9e6bdaa777d4
Murugadass-b <murugadassbala@gmail.com>
2023-05-22 20:34:53 +0530

    changes made in 30th program

--- java/Power.java
+++ java/Power.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Power {
-    public static void main(String args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double base;
         System.out.print("Enter the value of base:");

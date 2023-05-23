ffe23dc291528fc0c0607d12f8be40f2c34e35f4
Murugadass-b <murugadassbala@gmail.com>
2023-05-23 20:38:25 +0530

    33th program added

--- java/PerfectNums.java
+++ java/PerfectNums.java
@@ -1,8 +1,17 @@
 //33
+import java.util.*;
 public class PerfectNums {
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-
+        System.out.print("Enter the number:");
         int num = sc.nextInt();
+        int sum =0;
+        for(int i = 1;i<num;i++){
+            if(num%i==0){
+                sum += i;
+            }
+        }
+        System.out.println((sum==num)?num+" is a perfect number":num+" is not a perfect number");
+        sc.close();
     }
 }
